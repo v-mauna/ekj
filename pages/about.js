f5e23fa22aed3c1b25f122b67d07c8423a6f8ad9
Vanessa Mauna <vmauna@aol.com>
2020-05-26 16:46:47 -0400

    Updated EKFL image path in about.js

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
-import EKFL from '../photos/CrimeCon.jpg'
+import EKFL from './images/CrimeCon.jpg'
 
 const About = () => (
   <Layout>
